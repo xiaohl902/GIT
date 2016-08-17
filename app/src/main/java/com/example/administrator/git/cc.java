92d109d4ef5a68ffcb4923e5999d5987828f4c47
Administrator <1205427587@qq.com>
2016-08-17 16:29:16 +0800

    白术

--- app/src/main/java/com/example/administrator/git/cc.java
+++ app/src/main/java/com/example/administrator/git/cc.java
@@ -1,8 +1,28 @@
 package com.example.administrator.git;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
 /**
  * Created by Administrator on 2016/8/17 0017. on 16:08
  * xiaohl_902
  */
-public class cc {
+public class cc extends TextView{
+
+
+    public cc(Context context) {
+        super(context);
+    }
+
+    public cc(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public cc(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
 }
